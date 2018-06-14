593d55c265f19b20d3a0165fc44cce7ca88821df
carpon <carpon39038@163.com>
2018-06-14 16:53:17 +0800

    feat:增加了一个select选项 增加了json数据 修改了filter样式

--- src/Filter.js
+++ src/Filter.js
@@ -4,10 +4,19 @@ import './Filter.css';
 class Filter extends React.Component {
     render(){
         return(
-            <select id="filter">
-                <option value="restaurant">食堂</option>
-                <option value="classroom">教学楼</option>
-            </select>
+            <div id="filter">
+                <select id="block">
+                    <option value="all">所有校区</option>
+                    <option value="north">北校区</option>
+                    <option value="south">南校区</option>
+                </select>
+                <select id="type">
+                    <option value="all">所有建筑</option>
+                    <option value="restaurant">食堂</option>
+                    <option value="classroom">教学楼</option>
+                </select>
+
+            </div>
         )
     }
 }
