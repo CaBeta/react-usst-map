7c98cf65f2c3e7e5de5970e1af4eb3151dec72bf
carpon <carpon39038@163.com>
2018-06-14 20:49:19 +0800

    feat:增加filter的onChange事件 实现filter选项过滤内容

--- src/App.js
+++ src/App.js
@@ -5,6 +5,45 @@ import List from './List'
 import Filter from './Filter'
 
 class App extends Component {
+  state = {
+    defaultLocation:[],
+    location:[],
+    block:'',
+    type:''
+  }
+  componentDidMount(){
+    fetch("http://localhost:3000/data/location.json").then((response)=>{
+      return response.json();
+    }).then((data)=>{
+      this.setState({
+        defaultLocation:data.location,
+        location:data.location
+      });
+    })
+    .catch((error)=>{
+      console.log(error);
+    })
+  }
+  onChangeBlock= (block) => {
+    this.setState({
+      block:block
+    })
+    if (block === "all") {
+      this.setState({
+        block: ""
+      })
+    }
+  }
+  onChangeType= (type) => {
+    this.setState({
+      type:type
+    })
+    if (type === "all") {
+      this.setState({
+        type: ""
+      })
+    }
+  }
   render() {
     return (
       <div className="App">
@@ -12,8 +51,9 @@ class App extends Component {
           <h1 className="App-title">Welcome to USST</h1>
         </header>
         <Map/>
-        <Filter/>
-        <List/>
+        <Filter changeBlock={this.onChangeBlock} changeType={this.onChangeType}/>
+        <List location={this.state.location.filter((item)=>this.state.block === '' || item.block === this.state.block)
+          .filter((item) => this.state.type === '' || item.type === this.state.type)}/>
       </div>
     );
   }
--- src/List.js
+++ src/List.js
@@ -8,22 +8,11 @@ class List extends React.Component {
     render(){
         return(
             <div id="location-list">
-                <div className="location-item">
-                    <h2>一食堂</h2>
-                    <p>有一楼和二楼，二楼有面条，里面有高端餐厅虽然我没进去过</p>
+            {this.props.location.map((item)=>(
+                <div className="location-item" key={item.id}>
+                    <h2>{item.name}</h2>
                 </div>
-                <div className="location-item">
-                    <h2>二食堂</h2>
-                    <p>一楼下课挤爆，二楼中午是教师食堂</p>
-                </div>
-                <div className="location-item">
-                    <h2>五食堂</h2>
-                    <p>很大，二楼很多小吃</p>
-                </div>
-                <div className="location-item">
-                    <h2>六食堂</h2>
-                    <p>比较大，旁边有两个小食堂，好像也有二楼但是我没去过</p>
-                </div>
+            ))}
             </div>
         );
     }
